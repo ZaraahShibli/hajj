2f9acb6649abfc73ef47887c21425c4d0a0dfe4e
ZaraahShibli <42017072+ZaraahShibli@users.noreply.github.com>
2018-08-02 17:35:17 +0300

    Initial commit

---
+++ app/src/main/java/com/example/zaraah/hajj/PastStuations.java
@@ -0,0 +1,13 @@
+package com.example.zaraah.hajj;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PastStuations extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.past_stuation);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/zaraah/hajj/Profile.java
@@ -0,0 +1,13 @@
+package com.example.zaraah.hajj;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.zaraah.hajj.R;
+
+public class Profile extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}
